7b4e152d090a7b880791b584e19b66fa348f7339
agarwalarjun123 <arjunagarwal1998@gmail.com>
2019-03-23 13:52:36 +0530

    added ssh login

--- helpers/ssh.js
+++ helpers/ssh.js
@@ -2,11 +2,10 @@ const {exec} = require('child_process')
 const fs = require('fs') 
 const setConnection = (user)=>{
     return new Promise((resolve,reject)=>{
-            exec(`sshpass -p '${user.password}' ssh ${user.name}@${user.ip} 'df -h'`,(err,stdout,stderr)=>{
+            exec(`sshpass -p '${user.password}' ssh ${user.name}@${user.ip} './init.sh'`,(err,stdout,stderr)=>{
                 if(err)
                     console.log(err)
                 console.log(stdout)
-                console.log(stderr)
                 
             })
             

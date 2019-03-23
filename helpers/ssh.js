7095ce811639d37311a4e7a48a762528154fd330
agarwalarjun123 <arjunagarwal1998@gmail.com>
2019-03-23 05:41:45 +0530

    finished docker code

--- helpers/ssh.js
+++ helpers/ssh.js
@@ -1,15 +1,14 @@
 const {spawn} = require('child_process')
 const setConnection = (user)=>{
+
     return new Promise((resolve,reject)=>{
-        let child = spawn('ssh',[],{
-            shell:true
-        })
+        let child = spawn('ssh',['-o','StrictHostKeyChecking=no','-tt',`${user.name}@${user.ip}`])
         child.stdout.on('data', function(data) {
-            console.log('stdout: ' + data);
-          });
+            resolve(data)
+        });
           child.stderr.on('data', function(data) {
-            console.log('stderr: ' + data);
-          });
+            reject(data)
+        });
           
 
 })
--- helpers/util.js
+++ helpers/util.js
@@ -1,21 +1,68 @@
 const {exec} = require('child_process')
+
 const running_containers = ()=>{
     return new Promise((resolve,rejects)=>{
-        exec('sudo docker ps',(err,stdout,stderr)=>{
+        exec(`sudo docker ps | awk '{print $1}'`,(err,stdout,stderr)=>{
             if(err)
                 return reject(new Error(err))
-            resolve(stdout)
+            resolve(stdout.split('\n').slice(1,-1))
         })
     })
+
 }
-const resource_util = (container_id)=>{
+
+const docker_systeminfo = ()=>{
     return new Promise((resolve,reject)=>{
-        exec(`sudo docker stats ${container_id}`,(err,stdout,stderr)=>{
+        let d={}
+        exec(`sudo docker system info`,(err,stdout,stderr)=>{
+            if(err)
+                return reject(new Error(err))
+            let data = stdout.split('\n')
+
+            data = data.filter(e => e)
+            data = data.map(e => {
+                d[e.split(':')[0]] = e.split(':')[1] 
+                return e
+            })
+            resolve(d)
+        })        
+    })
+}
+
+const docker_container_info = (docker_id)=>{
+    return new Promise((resolve,reject)=>{
+        exec(`sudo docker stats ${docker_id} --no-stream | awk '{print $1}{print $4}{print $7}{print $3}'`,(err,stdout,stderr)=>{
+            if(err)
+                return reject(new Error(err))
+            let data = stdout.slice(stdout.indexOf('USAGE')+5)
+
+            data = data.split('\n')
+            data = data.filter(e => e)
+            let obj={}
+            obj['container_id'] = data[0]
+            obj['ram'] = data[1]
+            obj['utilization'] = data[2]    
+            obj['cpu'] =  data[3]         
+            resolve(obj)
+        })
+    })
+}
+
+const docker_logs = (docker_id) => {
+    return new Promise((resolve,reject)=>{
+        exec(`sudo docker logs -f ${docker_id}`,(err,stdout,stderr)=>{
             if(err)
                 return reject(new Error(err))
             resolve(stdout)
-        })
-        
+
+        })   
     })
+   
 }
-resource_util('bc7').then(console.log).catch(console.log)
+
+module.exports={
+ docker_logs,
+ running_containers,
+ docker_systeminfo,
+ docker_container_info   
+}
